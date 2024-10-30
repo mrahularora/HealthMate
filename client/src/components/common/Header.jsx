1023e98aec2279766c729c2470e531fcd14b7fc6
Rahul Arora <rahularora07@hotmail.com>
2024-10-30 12:15:35 -0400

    Navigation Routes For Logged In User

--- client/src/components/common/Header.jsx
+++ client/src/components/common/Header.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from 'react-router-dom'; 
+import { Link, useNavigate } from 'react-router-dom'; 
+import { AuthContext } from '../../context/AuthContext'; 
 
 const Header = () => {
-    return (
-        <div className="header" bg="light">
-        <Navbar expand="lg">
-            <Navbar.Brand as={Link} to="/"><img src="/assets/images/logo2.png" alt="logo" className="width25" /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/about">About</Nav.Link>
-                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-                    <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                    <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
-        </div>
-    );
+  const { user, logout } = useContext(AuthContext); // Access user and logout function from context
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate('/'); // Redirect to the homepage after logging out
+  };
+
+  return (
+    <div className="header" bg="light">
+      <Navbar expand="lg">
+        <Navbar.Brand as={Link} to="/">
+          <img src="/assets/images/logo2.png" alt="logo" className="width25" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            
+            {/* Conditionally render based on whether the user is logged in or not */}
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/profile">Profile</Nav.Link>
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/signup">Sign Up</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Header;
