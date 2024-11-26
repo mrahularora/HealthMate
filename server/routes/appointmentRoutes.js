bd9edd004cef45a87ebac39ff33248c8c6d7fc7c
Rahul Arora <rahularora07@hotmail.com>
2024-11-26 17:10:35 -0500

    Doctor Portal Back-End , API- Routes Requested Appointments, All Appointments, Appointment Details

--- server/routes/appointmentRoutes.js
+++ server/routes/appointmentRoutes.js
@@ -9,6 +9,24 @@ router.post("/create", appointmentController.createAppointmentSlots);
 // Route to get available slots for a doctor on a specific date
 router.post("/available", appointmentController.getAvailableSlots);
 
+// Route to book an appointment slot
+router.post("/book-request", appointmentController.bookAppointmentRequest);
+
+// Route to get requested Appointments
+router.get("/requests", appointmentController.getAppointmentRequests);
+
+// Route to accept or reject appointment by doctor
+router.patch("/status", appointmentController.updateAppointmentStatus);
+
+// Route for accepted appointments
+router.get("/accepted", appointmentController.getAcceptedAppointments);
+
+// Get appointment detail page
+router.get("/details", appointmentController.getAppointmentDetails);
+
+// Update Prescription
+router.patch("/details", appointmentController.updateAppointmentDetails);
+
 // // Route to get appointments for the logged-in user
 // router.get('/', protect, appointmentController.viewAppointments);
 
