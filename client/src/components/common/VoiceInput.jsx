664e156cfead526dd67fcd79184919bc0f96b511
Rahul Arora <rahularora07@hotmail.com>
2026-07-12 06:00:41 -0400

    Restore microphone icon in voice input

--- client/src/components/common/VoiceInput.jsx
+++ client/src/components/common/VoiceInput.jsx
@@ -55,7 +55,16 @@ const VoiceInput = ({
         onClick={handleVoiceInput}
         type="button"
       >
-        Mic
+        <svg
+          aria-hidden="true"
+          focusable="false"
+          height="18"
+          viewBox="0 0 24 24"
+          width="18"
+        >
+          <path d="M12 14c1.66 0 3-1.34 3-3V6c0-1.66-1.34-3-3-3S9 4.34 9 6v5c0 1.66 1.34 3 3 3Z" />
+          <path d="M17.5 11c0 3.03-2.47 5.5-5.5 5.5S6.5 14.03 6.5 11H5c0 3.58 2.72 6.53 6.2 6.92V21h1.6v-3.08c3.48-.39 6.2-3.34 6.2-6.92h-1.5Z" />
+        </svg>
       </button>
     </div>
   );
